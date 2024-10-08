a171707c456c3543018232e2e198b0fad8ee814e
Giacomo Nanni <gn.nanni@gmail.com>
2024-10-08 11:04:34 +0200

    chore: videos replaced

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -2,7 +2,7 @@
     <iframe
         width="100%"
         height="80%"
-        src="https://www.youtube.com/embed/KBW74piYk2k?controls=1&modestbranding=1&rel=0&showinfo=0"
+        src="https://www.youtube.com/embed/esvn44GEoAk?controls=1&modestbranding=1&rel=0&showinfo=0"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
